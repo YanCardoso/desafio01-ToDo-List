b940290a48a531f0b51d304d2b11ef2e116b9e36
Yan Cardoso <yancard7@gmail.com>
2022-09-15 14:45:45 -0300

    global style

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import styles from './App.module.scss'
+import logo from './logo.png'
+import './styles/global.scss'
+
+function App() {
+
+  return (
+    <header className={styles.container}>
+    </header>
+  )
+}
+
+export default App
